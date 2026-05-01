b4f7019b3b097c099a0f2958ca4bdaee0aae67f5
Ricardo <ricardosneres22@gmail.com>
2026-05-01 17:12:14 -0300

    funções base do user_service

--- services/user_service.py
+++ services/user_service.py
@@ -1,6 +1,21 @@
-from db_service import salvar_db, carregar_db
+from db_service import salvar_db, carregar_db, salvar_usuario
 from models.user_model import Usuario
+from services.db_service import buscar_usuario_email
+
 
 def criar_usuario(email, senha, telefone):
-    usuario = Usuario(email, senha, telefone)
+    if buscar_usuario_email(email):
+        raise FileExistsError("Email já existente.")
+
+    user = Usuario(email=email, senha=senha, telefone=telefone)
+    salvar_usuario(user)
     return True
+
+def login_user(email, senha):
+    db = carregar_db()
+
+    for usuario in db["users"]:
+        if usuario["email"] == email and usuario["senha"] == senha:
+            return True
+
+    return False
