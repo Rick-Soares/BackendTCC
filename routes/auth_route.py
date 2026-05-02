2a4b07c5ae8db2428412abd8108b17b15fb87353
Ricardo <ricardosneres22@gmail.com>
2026-05-02 08:59:40 -0300

    separando auth_route de device_route

--- routes/auth_route.py
+++ routes/auth_route.py
@@ -1,7 +1,5 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from services.user_service import criar_usuario, login_user
-from services.db_service import registrar_dispositivo
-from fastapi import HTTPException
 from models.user_model import Usuario, LoginRequest
 
 auth_router = APIRouter(prefix="/auth", tags=["Autenticação"])
@@ -17,16 +15,6 @@ async def novo_usuario(data: Usuario):
         "mensagem": "Usuário cadastrado com sucesso."
         }
 
-@auth_router.post("/registrar-dispositivo")
-async def novo_dispositivo(email_usuario: str):
-    try:
-        registrar_dispositivo(email_usuario)
-    except FileNotFoundError as e:
-        raise HTTPException(status_code=400, detail=str(e))
-    else:
-        return {
-            "mensagem": "Dispositivo cadastrado com sucesso."
-        }
 
 @auth_router.post("/login")
 async def login_usuario(data: LoginRequest):
--- routes/device_route.py
+++ routes/device_route.py
@@ -1,3 +1,15 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from services.db_service import registrar_dispositivo
 
 device_router = APIRouter(prefix="/order", tags=["Dispositivo"])
+
+@device_router.post("/registrar-dispositivo")
+async def novo_dispositivo(email_usuario: str):
+    try:
+        registrar_dispositivo(email_usuario)
+    except FileNotFoundError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    else:
+        return {
+            "mensagem": "Dispositivo cadastrado com sucesso."
+        }
