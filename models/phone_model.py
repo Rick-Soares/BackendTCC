bee97b7c1d066d449bb8d1785a53249cd585e7d6
Ricardo <ricardosneres22@gmail.com>
2026-05-02 15:40:36 -0300

    criação do model Telefone

---
+++ models/phone_model.py
@@ -0,0 +1,21 @@
+from datetime import datetime, UTC
+from pydantic import BaseModel
+from uuid import uuid4
+
+class CriarTelefone(BaseModel):
+    numero: str
+
+class Telefone(BaseModel):
+    telefone_id: str
+    user_id: str
+    numero: str
+    created_at: datetime
+
+    @classmethod
+    def criar(cls, data: CriarTelefone, user_id: str):
+        return cls(
+            telefone_id = str(uuid4()),
+            user_id = user_id,
+            numero = data.numero,
+            created_at = datetime.now(UTC)
+        )
