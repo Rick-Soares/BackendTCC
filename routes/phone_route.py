181446f0f08672390e95e719fd864c5a4a1962a5
Ricardo <ricardosneres22@gmail.com>
2026-05-02 16:21:32 -0300

    criação rotas de telefone

--- routes/phone_route.py
+++ routes/phone_route.py
@@ -5,7 +5,27 @@ from services.phone_service import (
     listar_telefones,
     remover_telefone
 )
-from pydantic import BaseModel
 
 phone_router = APIRouter(prefix="/phones", tags=["Telefones"])
+#user id para testes
+USER_ID_FIXO = "123"
 
+@phone_router.post("/add-phone")
+async def novo_telefone(data: TelefoneRequest):
+    try:
+        return criar_telefone(data.numero, USER_ID_FIXO)
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+@phone_router.get("/list-phones")
+async def listar():
+    return listar_telefones(USER_ID_FIXO)
+
+
+@phone_router.delete("/{telefone_id}")
+async def deletar(telefone_id: str):
+    try:
+        return remover_telefone(telefone_id, USER_ID_FIXO)
+    except ValueError as e:
+        raise HTTPException(status_code=404, detail=str(e))
--- services/device_service.py
+++ services/device_service.py
@@ -1,10 +1,11 @@
 from models.device_model import CriarDevice, Device
 from services.db_service import salvar_dispositivo
 
+USER_ID_FIXO = "123"
 
 def criar_dispositivo(nome_dispositivo, tipo_dispositivo):
     data = CriarDevice(device_name=nome_dispositivo, device_type=tipo_dispositivo)
-    dispositivo = Device.criar(data=data, user_id="123")
+    dispositivo = Device.criar(data=data, user_id=USER_ID_FIXO)
 
     salvar_dispositivo(dispositivo)
 
