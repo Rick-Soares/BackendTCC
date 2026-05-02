710809c08372e8b73add5f016881fb3b0f468018
Ricardo <ricardosneres22@gmail.com>
2026-05-02 16:09:15 -0300

    listar e deletar telefones funcionando

--- services/phone_service.py
+++ services/phone_service.py
@@ -1,5 +1,5 @@
 from models.phone_model import CriarTelefone, Telefone
-from services.db_service import salvar_telefone, verificar_telefone
+from services.db_service import salvar_telefone, verificar_telefone, buscar_telefones_por_usuario, deletar_telefone
 
 def criar_telefone(numero, user_id):
     if not numero.isdigit():
@@ -16,4 +16,22 @@ def criar_telefone(numero, user_id):
     "mensagem": "Telefone cadastrado com sucesso."
 }
 
+def listar_telefones(user_id):
+    telefones = buscar_telefones_por_usuario(user_id)
 
+    return {
+        "telefones": telefones
+    }
+
+
+def remover_telefone(telefone_id, user_id):
+    sucesso = deletar_telefone(telefone_id, user_id)
+
+    if not sucesso:
+        raise ValueError("Telefone não encontrado.")
+
+    return {
+        "mensagem": "Telefone removido com sucesso."
+    }
+
+print(remover_telefone("9b654f5b-d514-46af-b4c8-3d54ca8d2a02", "123456"))
