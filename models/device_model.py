c3b03f9fe434aa502ea3bbef639cee09c42a6aee
Ricardo <ricardosneres22@gmail.com>
2026-05-02 09:37:46 -0300

    refatorando a classe Device

--- models/device_model.py
+++ models/device_model.py
@@ -1,12 +1,27 @@
-from uuid import uuid8
-from pydantic import BaseModel, Field
+from uuid import uuid4
+import secrets
+from pydantic import BaseModel
+from datetime import datetime, UTC
+
+class CriarDevice(BaseModel):
+    device_name: str
+    device_type: str
 
 class Device(BaseModel):
-    device_id: str = Field(default_factory=uuid8)
-    email_usuario: str
+    device_id: str
+    device_name: str
+    device_type: str
+    device_token: str
+    user_id: str
+    created_at: datetime
 
-    def to_dict(self):
-        return {
-            "device_id": self.device_id,
-            "email_usuario": self.email_usuario,
-        }
+    @classmethod
+    def criar(cls, data: CriarDevice, user_id: str):
+        return cls(
+            device_id = str(uuid4()),
+            device_name = data.device_name,
+            device_type = data.device_type,
+            device_token = secrets.token_hex(16),
+            user_id = user_id,
+            created_at = datetime.now(UTC),
+        )
