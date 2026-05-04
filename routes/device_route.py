bca849c33d81a6e5d03aa1c74d610a8f76e44433
Ricardo <ricardosneres22@gmail.com>
2026-05-04 19:02:17 -0300

    add token jwt para criar dispositivo

--- routes/device_route.py
+++ routes/device_route.py
@@ -1,13 +1,14 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Depends
 from services.device_service import criar_dispositivo
 from models.device_model import CriarDevice
+from auth.dependencias_auth import verificar_token
 
 device_router = APIRouter(prefix="/devices", tags=["Dispositivo"])
 
 @device_router.post("/registrar-dispositivo")
-async def novo_dispositivo(data: CriarDevice):
+async def novo_dispositivo(data: CriarDevice, user_id : str = Depends(verificar_token)):
     try:
-        resposta = criar_dispositivo(data.device_name, data.device_type)
+        resposta = criar_dispositivo(data.device_name, data.device_type, user_id)
     except FileNotFoundError as e:
         raise HTTPException(status_code=400, detail=str(e))
     else:
--- services/device_service.py
+++ services/device_service.py
@@ -1,11 +1,11 @@
 from models.device_model import CriarDevice, Device
 from services.db_service import salvar_dispositivo
 
-USER_ID_FIXO = "123"
 
-def criar_dispositivo(nome_dispositivo, tipo_dispositivo):
+
+def criar_dispositivo(nome_dispositivo, tipo_dispositivo, user_id):
     data = CriarDevice(device_name=nome_dispositivo, device_type=tipo_dispositivo)
-    dispositivo = Device.criar(data=data, user_id=USER_ID_FIXO)
+    dispositivo = Device.criar(data=data, user_id=user_id)
 
     salvar_dispositivo(dispositivo)
 
