5aa08fcd45650269e6edfaec95307b0d86cc2135
Ricardo <ricardosneres22@gmail.com>
2026-05-04 18:18:44 -0300

    criação de token jwt

---
+++ auth/dependencias_auth.py
@@ -0,0 +1,15 @@
+from fastapi import Depends, HTTPException
+from fastapi.security import HTTPBearer
+from auth.jwt_auth import decodificar_token
+
+security = HTTPBearer()
+
+def verificar_token(credentials = Depends(security)):
+    token = credentials.credentials
+
+    try:
+        user_id = decodificar_token(token)
+        return user_id
+
+    except Exception:
+        raise HTTPException(status_code=401, detail="Token inválido")
---
+++ auth/jwt_auth.py
@@ -0,0 +1,35 @@
+from datetime import datetime, timedelta, UTC
+from jose import jwt,JWTError
+
+# (jogar em.env)
+chave_secreta = "segredo-completo"
+algoritmo = "HS256"
+expira_em = 60
+
+
+def criar_token(user_id: str) -> str:
+    expire = datetime.now(UTC) + timedelta(minutes=expira_em)
+
+    payload = {
+        "sub": user_id,   # subject (quem é o usuário)
+        "exp": expire     # tempo de expiração
+    }
+
+    token = jwt.encode(payload, chave_secreta, algorithm=algoritmo)
+
+    return token
+
+
+def decodificar_token(token: str) -> str:
+    try:
+        payload = jwt.decode(token, chave_secreta, algorithms=[algoritmo])
+
+        user_id = payload.get("sub")
+
+        if user_id is None:
+            raise Exception("Token inválido")
+
+        return user_id
+
+    except JWTError:
+        raise Exception("Token inválido ou expirado")
